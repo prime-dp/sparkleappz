3b05dfd816790c01f945fd4cebdce173bab6886f
prime-dp <59540914+prime-dp@users.noreply.github.com>
2020-02-06 04:20:40 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    //шапка
+    $(window).scroll(function () {
+
+        let wScroll = $(this).scrollTop();
+
+        if (wScroll > 30) {
+            $('.header-wrapper').addClass('header-active');
+        } else {
+            $('.header-wrapper').removeClass('header-active');
+        }
+
+    });
+
+    $(".owl-carousel").owlCarousel({
+        items: 5,
+        nav: true,
+        autoplay: true,
+        loop: true,
+        navText : ["",""],
+        navContainer: '.clients-carousel-wrap',
+        dots: false,
+        margin: 10
+    });
+});
+$(window).on("load", function () {
+    //анимация
+    wow = new WOW(
+        {
+            boxClass:     'wow',      // default
+            animateClass: 'animated', // default
+            offset:       0,          // default
+            live:         true
+        }
+    );
+
+    wow.init();
+});
