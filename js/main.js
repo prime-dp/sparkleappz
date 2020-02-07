2149af62ec982a875024ae158fa1e56128bc7299
prime-dp <59540914+prime-dp@users.noreply.github.com>
2020-02-07 11:38:55 +0200

    portfolio,contacts,footer

--- js/main.js
+++ js/main.js
@@ -39,12 +39,104 @@ $(document).ready(function() {
     $('.customNextBtn').click(function() {
         owl.trigger('next.owl.carousel');
     })
-// Go to the previous item
+    // Go to the previous item
     $('.customPrevBtn').click(function() {
         // With optional speed parameter
         // Parameters has to be in square bracket '[]'
         owl.trigger('prev.owl.carousel', [300]);
-    })
+    });
 
     wow.init();
+
+    //валидация форм
+    let pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i,
+        mailInput = $('#input-email'),
+        nameInput = $('#input-name'),
+        textInput = $('#input-message');
+
+    $('#contacts-form').submit(function(event){
+        $('.form-control').removeClass('invalid-input');
+        if(mailInput.val() != '' || nameInput.val() != '' || textInput.val() != ''){
+            if(mailInput.val().search(pattern) == 0){
+                //alert("алё");
+                mailInput.removeClass('invalid-input');
+            }else{
+                //alert("не алё");
+                mailInput.addClass('invalid-input');
+            }
+            if(nameInput.val() != ''){
+                $(this).removeClass('invalid-input');
+            }else {
+                $(this).addClass('invalid-input');
+            }
+            if(textInput.val() != ''){
+                $(this).removeClass('invalid-input');
+            }else {
+                $(this).addClass('invalid-input');
+            }
+        }else{
+
+            //alert("пусто");
+            $('.form-control').addClass('invalid-input');
+        }
+        event.preventDefault();
+    });
+
+    //phone mask
+
+    //$("#input-phone").mask("+3 (000) 000-0000");
+
+
+    //scrollspy
+
+    // function Scroll_block() {
+    //     let scrolled = $(document).scrollTop(),
+    //         windowHeight = $(window).height() / 2;  //высота окна
+    //
+    //     $("#top-menu a").each(function () {
+    //         let hash = $(this).attr("href"),
+    //             target = $(hash),
+    //             mainContent = $("#main-content");
+    //
+    //         if (mainContent.offset().top < windowHeight + scrolled) {
+    //             $("#link-start").addClass("show-link");
+    //             $('#contacts-list').addClass('show-contacts');
+    //         } else {
+    //             $("#link-start").removeClass("show-link");
+    //             $('#contacts-list').removeClass('show-contacts');
+    //         }
+    //
+    //         if ($(window).width() > 991) {
+    //             if (target.position().top - windowHeight <= scrolled && target.position().top + target.outerHeight() - windowHeight > scrolled) {
+    //                 $("#top-menu li.active").parent().removeClass("active");
+    //                 $(this).parent().addClass("active");
+    //             } else {
+    //                 $(this).parent().removeClass("active");
+    //             }
+    //         }
+    //     });
+    // }
+    //
+    // $(document).on("scroll", Scroll_block);
+    //
+    // $("a[href*=\\#]").click(function(e){
+    //     e.preventDefault();
+    //     $(document).off("scroll");
+    //
+    //     //Подсветка пунктов шапки в мобайле
+    //     if ($(window).width() > 991) {
+    //         $("#top-menu li.active").removeClass("active");
+    //         $(this).parent().addClass("active");
+    //     }
+    //
+    //     let hash = $(this).attr("href"),
+    //         target = $(hash);
+    //
+    //     $("html, body").animate({
+    //         scrollTop: target.offset().top
+    //     }, 800, function(){
+    //         window.location.hash = hash;
+    //         $(document).on("scroll", Scroll_block);
+    //     });
+    // });
 });
